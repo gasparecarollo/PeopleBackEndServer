b671e80b3af111695c9865d7c84ed2253f2e359a
Gaspare Carollo <gasparecarollo@pursuit.org>
2024-12-04 13:52:14 -0500

    Adjusted the controllers and implemented 'getAllPeople'

--- controller/people.js
+++ controller/people.js
@@ -1,71 +1,84 @@
 const express = require("express");
 const peopleArr = require("../data/people");
 const people = express.Router();
+const { getAllPeople } = require("../queries/people");
 
-people.get("/people", (req, res) => {
-    res.status(200).json(peopleArr);
-})
-
-people.get("/:id", (req, res) => {
+people.get("/", async (req, res) => {
     try {
-        const { id } = req.params;
-        const person = people.find((person) =>
-            person.id === Number(id));
-        if (person) {
-            res.status(200).json(person);
-        } else {
-            throw "Person could not be found";
-        }
+        const people = await getAllPeople()
+        res.status(200).json({ payload: people })
     } catch (error) {
-        res.status(404).json({ error: error })
+        res.status(404).json({ payload: error })
     }
-
-    people.post("/people", (req, res) => {
-        try {
-            const person = req.body;
-            person.id = peopleArr.length + 1;
-            if (person.name && person.description) {
-                peopleArr.push(person)
-                res.status(201).json(peopleArr[peopleArr.length - 1]);
-            }
-        } catch (error) {
-            res.status(400).json({ error: error })
-        }
-
-        people.delete("/:id", (req, res) => {
-            try {
-                const { id } = req.params;
-                const index = peopleArr.findIndex((person) =>
-                    person.id === Number(id));
-                if (index !== -1) {
-                    animesArr.splice(index, 1)
-                    res.status(200).json(peopleArr);
-                } else {
-                    throw "Person could not be found"
-                }
-            } catch (error) {
-                res.status(400).json({ error: error });
-            }
-        })
-    })
-
-    people.put("/:id", (req, res) => {
-        try {
-
-            const { id } = req.params;
-            const person = req.body;
-            const index = peopleArr.findIndex((person) => person.id === Number(id));
-            if (index !== -1) {
-                peopleArr.splice(index, 1, person);
-                res.status(200).json(peopleArr);
-            } else {
-                throw "Could not update person";
-            }
-        } catch (error) {
-            res.status(400).json({ error: error });
-        }
-    })
 })
+
+
+
+
+// people.get("/people", (req, res) => {
+//     res.status(200).json(peopleArr);
+// })
+
+// people.get("/:id", (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const person = people.find((person) =>
+//             person.id === Number(id));
+//         if (person) {
+//             res.status(200).json(person);
+//         } else {
+//             throw "Person could not be found";
+//         }
+//     } catch (error) {
+//         res.status(404).json({ error: error })
+//     }
+
+//     people.post("/people", (req, res) => {
+//         try {
+//             const person = req.body;
+//             person.id = peopleArr.length + 1;
+//             if (person.name && person.description) {
+//                 peopleArr.push(person)
+//                 res.status(201).json(peopleArr[peopleArr.length - 1]);
+//             }
+//         } catch (error) {
+//             res.status(400).json({ error: error })
+//         }
+
+//         people.delete("/:id", (req, res) => {
+//             try {
+//                 const { id } = req.params;
+//                 const index = peopleArr.findIndex((person) =>
+//                     person.id === Number(id));
+//                 if (index !== -1) {
+//                     animesArr.splice(index, 1)
+//                     res.status(200).json(peopleArr);
+//                 } else {
+//                     throw "Person could not be found"
+//                 }
+//             } catch (error) {
+//                 res.status(400).json({ error: error });
+//             }
+//         })
+//     })
+
+//     people.put("/:id", (req, res) => {
+//         try {
+
+//             const { id } = req.params;
+//             const person = req.body;
+//             const index = peopleArr.findIndex((person) => person.id === Number(id));
+//             if (index !== -1) {
+//                 peopleArr.splice(index, 1, person);
+//                 res.status(200).json(peopleArr);
+//             } else {
+//                 throw "Could not update person";
+//             }
+//         } catch (error) {
+//             res.status(400).json({ error: error });
+//         }
+//     })
+// })
 
 
 
--- queries/people.js
+++ queries/people.js
@@ -1,1 +1,14 @@
-const db = require("")
+const db = require("../db/dbConfig");
+
+const getAllPeople = async () => {
+    try {
+        const getAllPeople = await db.any("SELECT * FROM people")
+        return people;
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = {
+    getAllPeople
+}
